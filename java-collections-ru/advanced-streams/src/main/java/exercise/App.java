6815c2993b8f5caeb8c88096ff938130f6db525a
@hexlet/cli <support@hexlet.io>
2023-12-27 09:34:32 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -12,28 +12,6 @@ import java.util.stream.Stream;
 // BEGIN
 public class App {
 
-    private static String data1;
-    private static String data2;
-
-    private static int counter = 0;
-
-    private static Path getFixturePath(String fileName) {
-        return Paths.get("src", "test", "resources", "fixtures", fileName)
-                .toAbsolutePath().normalize();
-    }
-
-    private static String readFixture(String fileName) throws Exception {
-        Path filePath = getFixturePath(fileName);
-        return Files.readString(filePath).trim();
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        data1 = readFixture("s1.conf");
-        data2 = readFixture("s2.conf");
-        System.out.println("\n\n" + getForwardedVariables(data2));
-    }
-
     public static String getForwardedVariables(String string) {
         var prefix = "X_FORWARDED_";
         Stream<String> stream = string.lines();
