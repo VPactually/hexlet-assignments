de6fa819c9b8ae98e5b7ce7eb250f214bc0f1553
@hexlet/cli <support@hexlet.io>
2024-01-12 19:30:39 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -5,19 +5,15 @@ import java.util.Map;
 
 // BEGIN
 public class App {
-    public static void main(String[] args) {
-        KeyValueStorage data = new InMemoryKV(Map.of("key", "10"));
-        swapKeyValue(data);
-    }
-
     public static void swapKeyValue(KeyValueStorage data) {
         var newMap = new HashMap<String, String>();
         var result = data.toMap();
         System.out.println(data);
-        result.forEach((k, v) -> newMap.put(v, k));
-        data.toMap().clear();
+        result.forEach((k, v) -> {
+            newMap.put(v, k);
+            data.unset(k);
+        });
         newMap.forEach(data::set);
-        System.out.println(data);
     }
 }
 // END
