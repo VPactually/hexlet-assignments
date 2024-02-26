29f6eb153555e42b43589b860db40959edbfcc6d
@hexlet/cli <support@hexlet.io>
2024-02-26 13:35:01 +0300

    submit java-spring-ru/entity-validation

---
+++ java-spring-ru/entity-validation/src/main/java/exercise/dto/GuestCreateDTO.java
@@ -0,0 +1,70 @@
+package exercise.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+// BEGIN
+public class GuestCreateDTO {
+
+    @NotNull
+    private String name;
+    @Email
+    private String email;
+    @Pattern(regexp = "^\\+\\d{11,13}$")
+    private String phoneNumber;
+    @Size(min = 4, max = 4)
+    private String clubCard;
+    @Future
+    private LocalDate cardValidUntil;
+    private LocalDate createdAt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getClubCard() {
+        return clubCard;
+    }
+
+    public void setClubCard(String clubCard) {
+        this.clubCard = clubCard;
+    }
+
+    public LocalDate getCardValidUntil() {
+        return cardValidUntil;
+    }
+
+    public void setCardValidUntil(LocalDate cardValidUntil) {
+        this.cardValidUntil = cardValidUntil;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+// END
